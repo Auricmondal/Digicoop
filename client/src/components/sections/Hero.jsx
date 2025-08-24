053ca54ab605cf75632538592abc16aa99f96508
Sunanda-05 <mannasunanda11705@gmail.com>
2025-08-25 01:20:35 +0530

    Fix hero section padding & badge width

--- client/src/components/sections/Hero.jsx
+++ client/src/components/sections/Hero.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import LeftSection from "./LeftHero";
 import RightSection from "./RightHero";
+import SectionWrapper from "../../layouts/SectionWrapper";
 
 const Hero = () => {
   return (
     <div className="md:grid grid-cols-7 min-h-[95vh] mx-auto">
-      <div className="col-span-4 h-full px-4 md:px-12">
-        <LeftSection />
+      <div className="col-span-4 h-full">
+        <SectionWrapper className="max-w-[1460px] mx-auto">
+          <LeftSection />
+        </SectionWrapper>
       </div>
       <div className="col-span-3 h-full">
         <RightSection />
