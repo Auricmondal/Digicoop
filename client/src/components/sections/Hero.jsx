95296365bc559f111ea25c1620d4b6d14d8891a4
Auric  Mondal <auricmondal@gmail.com>
2025-08-24 15:20:13 +0530

    fix - hero sections

--- client/src/components/sections/Hero.jsx
+++ client/src/components/sections/Hero.jsx
@@ -5,7 +5,7 @@ import RightSection from "./RightHero";
 const Hero = () => {
   return (
     <div className="md:grid grid-cols-7 min-h-[95vh] mx-auto">
-      <div className="col-span-4 h-full px-12">
+      <div className="col-span-4 h-full px-4 md:px-12">
         <LeftSection />
       </div>
       <div className="col-span-3 h-full">
--- client/src/components/ui/Buttons.jsx
+++ client/src/components/ui/Buttons.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export function ButtonFlippedReveal({ className, children, icon, hoverIcon }) {
   return (
     <button
-      className={`group cursor-pointer ${className}`}
+      className={`group cursor-pointer  ${className}`}
       style={{
         borderImage:
           "linear-gradient(93.63deg, rgba(255, 255, 255, 0.35) 3.45%, rgba(153, 153, 153, 0) 117.37%) 1",
@@ -13,11 +13,11 @@ export function ButtonFlippedReveal({ className, children, icon, hoverIcon }) {
         <span className="flex transform transition-transform duration-300 group-hover:-translate-y-full text-[15px] md:text-[20px]">
           {children}
         </span>
-        <span className="flex absolute left-0 top-0 transform translate-y-full transition-transform duration-300 group-hover:translate-y-0">
+        <span className="flex absolute left-0 top-0 transform translate-y-full transition-transform duration-300 group-hover:translate-y-0 text-[15px] md:text-[20px]">
           {children}
         </span>
       </span>
-        <span className="relative w-5 h-5">
+      <span className="relative w-5 h-5">
         <span className="absolute inset-0 transform transition-opacity duration-300 group-hover:opacity-0">
           {icon}
         </span>
