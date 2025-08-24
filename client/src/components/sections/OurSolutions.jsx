af1dae10576150d07a6a9d0ea88a48d80aa3cbed
AnuvabMaity <anuvabmaity99@gmail.com>
2025-08-24 18:41:49 +0530

    Added Our Solutions Section

--- client/src/components/sections/OurSolutions.jsx
+++ client/src/components/sections/OurSolutions.jsx
@@ -1,28 +1,44 @@
 import React from "react";
 import { SolutionCard } from "../ui/Cards";
 import { GradientBadge } from "../ui/Badges";
+import HeroImage from "../../assets/images/solutions.webp";
 
 const OurSolutions = () => {
   return (
-    <div className="bg-amber-100">
+    <div className="py-6 px-6">
       {/*Parent Background*/}
-      <div className="bg-cyan-100 flex flex-col justify-between items-center p-5">
+      <div className="flex flex-col items-center gap-4">
         <GradientBadge text="Our Solutions" />
 
-        <div className="">
-          {/* Title */}
-          Smarter Tools for Stronger <span>Cooperatives</span>
-        </div>
+        {/* Title */}
+        <h2 className="flex flex-col items-center justify-center text-center">
+          Smarter Tools for Stronger <br />
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-primary to-dark-tint font-semibold">
+            Cooperatives
+          </span>
+        </h2>
 
-        <div className="text-center">
-          {/* Subtitle */}
+        {/* Subtitle */}
+        <p className="text-center">
           Practical, compliant, and easy to use - our tools give cooperatives
           the clarity and control they need.
-        </div>
+        </p>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2">
-        {/* Responsive Grid*/}
-        <SolutionCard title="Digital Administration" subtitle="DigiV is your cooperative’s command center. Manage memberships, shares, documents, and governance processes online — fast, secure, and fully compliant. No more paperwork, just clarity and control." buttontext="Discover DigiV"/>
+
+      {/* Responsive Grid*/}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-4 md:mx-8">
+        <SolutionCard
+          image={HeroImage}
+          title={`Digital Administration`}
+          subtitle={`DigiV is your cooperative’s command center. Manage memberships, shares, documents, and governance processes online — fast, secure, and fully compliant. No more paperwork, just clarity and control.`}
+          buttontext={`Discover DigiV`}
+        />
+        <SolutionCard
+          image ={HeroImage}
+          title={`Audits Made Effortless`}
+          subtitle={`DigiAudit provides a structured, transparent audit trail. From document requests to final reports, every step is clear, traceable, and digital — saving your team valuable time and reducing audit costs.`}
+          buttontext={`Discover DigiAudit`}
+        />
       </div>
     </div>
   );
--- client/src/components/ui/Cards.jsx
+++ client/src/components/ui/Cards.jsx
@@ -1,32 +1,35 @@
 import React from "react";
 import { ButtonFlippedReveal } from "./Buttons";
+import { ArrowRight, ArrowUpRight } from "lucide-react";
 
-export function SolutionCard({image, title, subtitle, buttontext}){
-    return (
-        <div className="bg-[#EAF2FF] border border-gray-300 rounded-xl m-5"> {/* Parent */}
-            <div className="rounded-t-lg p-10"> {/* Image Section */}
-                <div className="bg-white w-20 h-20 rounded-lg" />
-                {image}
-                <div/>
-            </div>
+export function SolutionCard({ image, title, subtitle, buttontext }) {
+  return (
+    <div className="bg-[#EAF2FF] border border-gray-300 rounded-xl">
+      {/* Image Section */}
+      <div className="rounded-t-lg h-100 overflow-hidden">
+        <div
+          className="bg-cover bg-no-repeat h-120 w-150 rounded-lg transform translate-y-[4%] translate-x-[15%] hover:transform hover:scale-115 hover:translate-x-[9%] ease-in-out duration-500"
+          style={{ backgroundImage: `url(${image})` }}
+        />
+        <div />
+      </div>
 
-            {/* Content Section */}
-            <div className="bg-white rounded-b-xl p-2">
-                <div className="bg-green-100 px-3 py-1 m-1">
-                    {title}
-                </div>
-                <div className="bg-green-200 px-3 py-1 m-1">
-                    {subtitle}
-                </div>
-                <div className="">
-                    <ButtonFlippedReveal
-                    // icon={<ArrowRight />}
-                    // hoverIcon={<ArrowRight className="text-primary bg-white rounded-full"/>}
-                    className= "bg-[#EAF2FF] text-primary-dark rounded-2xl w-full h-[64px] border-[2px] flex items-center justify-center">
-                    {buttontext}
-                    </ButtonFlippedReveal>
-                </div>
-            </div>
+      {/* Content Section */}
+      <div className="bg-white rounded-b-xl p-6 hover:shadow-lg">
+        <h3 className="m-1">{title}</h3>
+        <p className="m-1">{subtitle}</p>
+        <div className="">
+          <ButtonFlippedReveal
+            icon={<ArrowRight />}
+            hoverIcon={
+              <ArrowUpRight className="text-primary bg-white rounded-full" />
+            }
+            className="bg-[#EAF2FF] text-primary-dark rounded-xl w-full h-[64px] border-[2px] flex items-center justify-center font-semibold"
+          >
+            {buttontext}
+          </ButtonFlippedReveal>
         </div>
-    );
-};
+      </div>
+    </div>
+  );
+}
