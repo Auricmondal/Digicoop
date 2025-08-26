86510644d44b67e6d93d9fd71723d8610c4686e5
Sunanda-05 <mannasunanda11705@gmail.com>
2025-08-26 18:21:02 +0530

    Legacy animated header added

--- client/src/components/sections/Legacy.jsx
+++ client/src/components/sections/Legacy.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import legacyImage from "../../assets/images/legacy.webp";
 import SectionWrapper from "../../layouts/SectionWrapper";
 import { QuoteCard } from "../ui/Cards";
+import AnimatedHeader from "../ui/AnimatedHeader";
 
 const LegacySection = () => {
   return (
     <SectionWrapper className="max-w-[1460px] mx-auto">
-      <h2 className="text-2xl md:text-4xl lg:text-5xl font-semibold text-center m-8">
-        Carrying the Legacy Forward
-      </h2>
+      <div className="m-8 flex justify-center">
+        <AnimatedHeader
+          words={[
+            { text: "Carrying", isGradient: false },
+            { text: "the", isGradient: false },
+            { text: "Legacy", isGradient: false },
+            { text: "Forward", isGradient: false },
+          ]}
+          className="text-2xl md:text-4xl lg:text-5xl font-semibold text-center justify-center mx-auto text-black"
+        />
+      </div>
 
       <p className="text-base md:text-2xl text-center md:text-start mb-4">
         The cooperative story is filled with innovators:
@@ -20,7 +29,11 @@ const LegacySection = () => {
         <QuoteCard quote="José Maria Arizmendiarrieta, who showed that entrepreneurship can be member-owned." />
       </div>
       <p className="text-gray-700 md:text-xl lg:text-2xl mb-8">
-        We see DigiCoop as the next step in this legacy. By offering audit-proof processes, simple participation channels, and automated workflows, we bring the cooperative idea into practice for the 21st century. For us, the cooperative idea remains what it has always been: a timeless promise that grows stronger through cooperation.
+        We see DigiCoop as the next step in this legacy. By offering audit-proof
+        processes, simple participation channels, and automated workflows, we
+        bring the cooperative idea into practice for the 21st century. For us,
+        the cooperative idea remains what it has always been: a timeless promise
+        that grows stronger through cooperation.
       </p>
 
       <div className="h-[564px] rounded overflow-hidden ">
