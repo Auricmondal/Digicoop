a23f896e2f5dbf330023db05a46ee9481c0e3696
AnuvabMaity <anuvabmaity99@gmail.com>
2025-08-26 17:31:12 +0530

    added legacy section

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import AboutHero from "../components/sections/AboutHero";
 import OurVision from "../components/sections/OurVision";
 import ContactUs from "../components/sections/Contact";
+import LegacySection from "../components/sections/Legacy";
+
 const AboutPage = () => {
   return (
     <div>
       <AboutHero />
       <OurVision />
+      <LegacySection />
       <ContactUs />
     </div>
   );
