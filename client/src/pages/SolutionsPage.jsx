dc3dcfac271cd164a6c9dfea51b17bd5afd47eab
Anix003 <baranish2003@gmail.com>
2025-08-28 01:38:14 +0530

    added words from CEO

--- client/src/pages/SolutionsPage.jsx
+++ client/src/pages/SolutionsPage.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import HowThisWorks from "@/components/sections/HowThisWorks";
 import SolutionsHero from "@/components/sections/SolutionsHero";
+import WordsFromCEO from "@/components/sections/WordsFromCEO";
+import Brands from "@/components/sections/Brands";
 
 const SolutionsPage = () => {
   return (
     <div>
       <SolutionsHero />
+      <Brands />
       <HowThisWorks isStraight={true} />
+      <WordsFromCEO />
     </div>
   );
 };
